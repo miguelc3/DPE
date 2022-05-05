b49832d610165264de62a43d30c12843b62ec3e7
miguelc3 <miguelcpereira@ua.pt>
2022-05-05 10:16:17 +0100

    Trained models

--- Code/Matlab_sinusoidal_patterns/patterns_code.m
+++ Code/Matlab_sinusoidal_patterns/patterns_code.m
@@ -65,30 +65,7 @@ a = imshow(im3);
 impixelinfo(a)
 
 
-% Number_Of_Cycles = 50;
-% A = 1;
-% B = 0.5;
-% f = 1; 
-% Sampling_Frequency = 100;
-% Start_Time = 0;
-% End_Time = Number_Of_Cycles*1/f;
-% Sampling_Period = 1/Sampling_Frequency;
-% t = (Start_Time: Sampling_Period: End_Time);
-% y2 = B+A*cos(2*pi*f*t);
-% 
-% subplot(3,2,3)
-% plot(t, y2, '-')
-% xlabel("t"); ylabel("Amplitude");
-% axis([Start_Time End_Time -1.2 1.2]);
-% 
-% % Vertical pattern
-% subplot(3,2,4)
-% Image_Height = 480;
-% im1 = repmat(y2,Image_Height,1);
-% im1 = imresize(im1, [480 640]);
-% a = imshow(im1);
-% impixelinfo(a)
-% title("Vertical")
+
 
 
 
